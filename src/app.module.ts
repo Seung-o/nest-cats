ae967a50eb212507a6666a932edab4cff4e1c705
SEUNGO <seungo@SEUNGOs-MacBook-Air.local>
2022-05-11 00:10:16 +0900

    add: mongodb setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    }),
+    CatsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
